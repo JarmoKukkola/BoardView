a8357c77b33c1ae946aa4291e2be783aa605740e
Jarmo Kukkola <last.do.app@gmail.com>
2019-05-26 11:04:02 +0200

    Migration to AndroidX.

--- app/src/main/java/com/allyants/boardviewexample/MainActivity.java
+++ app/src/main/java/com/allyants/boardviewexample/MainActivity.java
@@ -1,10 +1,8 @@
 package com.allyants.boardviewexample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 
 import com.allyants.boardview.BoardView;
 import com.allyants.boardview.Item;
